372d60bc856a21df59e6bd1d5125d07389de4548
fzz <fzz>
2021-10-02 19:11:34 +0800

    修改docker run的程序路径

--- run-qt-docker.sh
+++ run-qt-docker.sh
@@ -3,15 +3,15 @@ user=`who -s -u -q | awk 'NR==1' | cut -d ' ' -f 1 2>&1`
 echo "user is ${user}"
 
 # N.B. This is an important step any time you're running GUIs in containers
-xhost local:${user}
+xhost local:root
 
 # run image
 docker run -v /tmp/.X11-unix:/tmp/.X11-unix \
            -e DISPLAY=$DISPLAY \
            -v /dev/shm:/dev/shm \
-           -v ~/src:/home/${user} \
+           -v ~/src:/home/${user}/src \
            --device /dev/dri \
            --name qt_creator \
            --rm \
-           --entrypoint /opt/Qt5.12.6/Tools/QtCreator/bin/qtcreator \
+           --entrypoint /home/${user}/Qt5.12.6/Tools/QtCreator/bin/qtcreator \
            qt:latest
